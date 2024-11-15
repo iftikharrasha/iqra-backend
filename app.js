a2ebcd3cc5322b917445015fe27e50b6c0e3861e
iftikhar rasha <iftikhar.rasha@northsouth.edu>
2024-11-15 14:56:22 +0600

    server request body size scalled up 3

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const mongoDBRoute = require('./src/routes/iqra/mongo.route.js');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
 
 app.use("/api/iqra/book", iqraRoute)
 app.use("/api/iqra/mongo", mongoDBRoute)
