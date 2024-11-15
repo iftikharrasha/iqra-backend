0245e33e3c9d27640adaba4619fcf5f50d062acb
iftikhar rasha <iftikhar.rasha@northsouth.edu>
2024-11-15 15:06:29 +0600

    free tier handshaked

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoDBRoute = require('./src/routes/iqra/mongo.route.js');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '4mb' }));
 
 app.use("/api/iqra/book", iqraRoute)
 app.use("/api/iqra/mongo", mongoDBRoute)
